e1bc0b557da7a44fa4ab253a2ea0fcfe0757c65e
Fabiola Monfil <fmonfil@intelimetrica.com>
2017-10-18 18:48:30 -0500

    Add AJAX request with Axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,15 @@
+import axios from 'axios';
+
 const API_KEY = 'ee0b9fad26f95c726bfda38e324fce5d';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETH_WEATHER = 'FETH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETH_WEATHER
+  };
+}
